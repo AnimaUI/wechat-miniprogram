3af74af137941d6985e96101861dfcfabd87c91e
花夏 <liubiao0810@live.cn>
2020-03-29 18:13:29 +0800

    feat: 修改基础元素渐变背景颜色

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -21,7 +21,7 @@ Page({
                 icon: 'ani-icon-icon',
                 en: 'icon',
                 cn: '图标',
-                linear: 'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);'
+                linear: 'linear-gradient(120deg, #a18cd1 0%, #fbc2eb 100%);'
             },
             {
                 icon: 'ani-icon-btn',
@@ -33,26 +33,26 @@ Page({
                 icon: 'ani-icon-text',
                 en: 'text',
                 cn: '文本',
-                linear: 'linear-gradient(to left, #ffecd2 0%, #fcb69f 100%);'
+                linear: 'linear-gradient(120deg, #ffecd2 0%, #fcb69f 100%);'
             },
             {
                 icon: 'ani-icon-text-shadow',
                 en: 'textShadow',
                 cn: '文本阴影',
                 linear:
-                    'linear-gradient(to top, #ff9a9e 0%, #fecfef 99%, #fecfef 100%);'
+                    'linear-gradient(120deg, #ff9a9e 0%, #fecfef 99%, #fecfef 100%);'
             },
             {
                 icon: 'ani-icon-tag',
                 en: 'tag',
                 cn: '标签',
-                linear: 'linear-gradient(to left, #9ee6fe 0%, #52d2fe 100%);'
+                linear: 'linear-gradient(120deg, #9ee6fe 0%, #6cd9fd 100%);'
             },
             {
                 icon: 'ani-icon-portrait',
                 en: 'avatar',
                 cn: '头像',
-                linear: 'linear-gradient(-20deg, #f794a4 0%, #fdd6bd 100%);'
+                linear: 'linear-gradient(120deg, #f794a4 0%, #fdd6bd 100%);'
             },
             {
                 icon: 'ani-icon-progress',
@@ -64,19 +64,19 @@ Page({
                 icon: 'ani-icon-box-shadow',
                 en: 'boxShadow',
                 cn: '边框阴影',
-                linear: 'linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);'
+                linear: 'linear-gradient(120deg, #fbc2eb 0%, #a6c1ee 100%);'
             },
             {
                 icon: 'ani-icon-loading3',
                 en: 'loading',
                 cn: '加载',
-                linear: 'linear-gradient(to left, #f3ebe5 0%, #e2d1c3 100%);'
+                linear: 'linear-gradient(120deg, #f3ebe5 0%, #e2d1c3 100%);'
             },
             {
                 icon: 'ani-icon-badge',
                 en: 'badge',
                 cn: '徽标',
-                linear: 'linear-gradient(to right, #ffc3a0 0%, #ffafbd 100%);'
+                linear: 'linear-gradient(120deg, #ffc3a0 0%, #ffafbd 100%);'
             }
         ]
     },
