2bc978dbfa661f0cae03d0095967d949739b70f7
花夏 <liubiao0810@live.cn>
2020-03-29 17:00:34 +0800

    feat: 添加首页基础元素展示

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -3,7 +3,68 @@
 const app = getApp();
 Page({
     data: {
-        tabBar: app.globalData.tabBar
+        tabBar: app.globalData.tabBar,
+        list: [
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },{
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            },
+            {
+                icon: 'ani-icon-color',
+                en: 'color',
+                cn: '颜色',
+                linear: 'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);'
+            }
+        ]
     },
     onLoad() {
         wx.setNavigationBarColor({
