15f8b835caec2f127d88f6999fc3df39e2d0db3b
花夏 <liubiao0810@live.cn>
2020-04-15 16:31:11 +0800

    feat: 更新页面样式

--- tools/demo/pages/solution/index.js
+++ tools/demo/pages/solution/index.js
@@ -9,73 +9,18 @@ Page({
         colour,
         list: [{
                 icon: 'ani-icon-bar',
-                en: 'topBar',
-                cn: '顶部Bar'
+                en: 'drawer',
+                cn: '抽屉'
             },
             {
-                icon: 'ani-icon-menu',
-                en: 'menuBar',
-                cn: '底部Bar'
+                icon: 'ani-icon-bar',
+                en: 'animation',
+                cn: '动画'
             },
             {
-                icon: 'ani-icon-tab',
-                en: 'tab',
-                cn: 'tab导航'
-            },
-            {
-                icon: 'ani-icon-form',
-                en: 'form',
-                cn: '表 单'
-            },
-            {
-                icon: 'ani-icon-carousel',
-                en: 'swiper',
-                cn: '轮 播'
-            },
-            {
-                icon: 'ani-icon-steps',
-                en: 'steps',
-                cn: '步骤条'
-            },
-            {
-                icon: 'ani-icon-card',
-                en: 'card',
-                cn: '卡 片'
-            },
-            {
-                icon: 'ani-icon-time-line',
-                en: 'timeLine',
-                cn: '时间轴'
-            },
-            {
-                icon: 'ani-icon-modal',
-                en: 'modal',
-                cn: '模态框'
-            },
-            {
-                icon: 'ani-icon-list',
-                en: 'list',
-                cn: '列 表'
-            },
-            {
-                icon: 'ani-icon-date',
-                en: 'date',
-                cn: '日 期'
-            },
-            {
-                icon: 'ani-icon-time2',
-                en: 'time',
-                cn: '时 间'
-            },
-            {
-                icon: 'ani-icon-slider',
-                en: 'slider',
-                cn: '滑 块'
-            },
-            {
-                icon: 'ani-icon-rate',
-                en: 'rate',
-                cn: '评 分'
+                icon: 'ani-icon-bar',
+                en: 'comic',
+                cn: '动漫'
             }
         ]
     },
