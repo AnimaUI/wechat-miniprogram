476f8381ee3846a98b3de29ef9d8592123192663
花夏 <liubiao0810@live.cn>
2020-05-06 17:43:41 +0800

    feat: 🍣 添加进度条展示页

---
+++ tools/demo/pages/basic/progress/index.js
@@ -0,0 +1,14 @@
+// 获取应用实例
+const app = getApp();
+let {
+    colour
+} = require('../../../utils/colour');
+Page({
+    data: {
+        imgBaseUrl: app.imgBaseUrl,
+        colour
+    },
+    onLoad() {},
+    onShareAppMessage() {},
+    onShow() {}
+});
